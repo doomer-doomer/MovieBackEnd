b78f64967d4767eaef7d6bc2b2a1f5fd02ca4dc3
unknown <tejasgotavade@gmail.com>
2023-04-10 08:27:44 +0530

    Change in TimeoutPage

--- client/src/404Page.js
+++ client/src/404Page.js
@@ -1,42 +1,28 @@
 import { useRef } from "react";
 import { Link } from "react-router-dom";
 import Login from "./login";
+import Button from 'react-bootstrap/Button';
 
 export default function ErrorPage(props){
 
     return (
-        <div className="errorbody">
-         <figure>
-	<div class="sad-mac"></div>
-	<figcaption>
-		<span class="sr-text">Error 404: Not Found</span>
-		<span class="e"></span>
-		<span class="r"></span>
-		<span class="r"></span>
-		<span class="o"></span>
-		<span class="r"></span>
-		<span class="_4"></span>
-		<span class="_0"></span>
-		<span class="_4"></span>
-		<span class="n"></span>
-		<span class="o"></span>
-		<span class="t"></span>
-		<span class="f"></span>
-		<span class="o"></span>
-		<span class="u"></span>
-		<span class="n"></span>
-		<span class="d"></span>
-
-      
-	</figcaption>
+        <div className="totalerrorlay">
+			<img src="redbg.jpg" width="100%"></img>
+			<div className="errorlay">
+				<span><img src="expired.png" width="50px"></img>Session Expired</span>
+				<br></br>
+				<p>Your Session has been expired. Click below to login again.</p>
+				<Link to="/login"><Button className="mybtn">Login</Button></Link>
+				<div className="errorbtn">
+						
+						{/* <h1>{props.err}</h1> */}
+						
+						
+				</div>
+			</div>
+		</div>
     
-            <div className="errorbtn">
-                 {/* <h1>{props.err}</h1> */}
-                 <img src="hand-pointer.png" width="50px" ></img>
-                 <Link to="/login"><button className="mybtn">Session Timeout!</button></Link>
-            </div>
-</figure>
-            
-        </div>
+           
+
     )
 }
